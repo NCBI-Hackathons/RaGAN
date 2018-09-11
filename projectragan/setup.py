3bf60cec282ad1f6c62a06af88ecc8420497864b
p.samar.j@gmail.com <ubuntu@ip-172-16-243-193.ec2.internal>
2018-09-11 10:07:26 -0400

    moved from src to ragan

--- projectragan/setup.py
+++ projectragan/setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+#    name='src',
+    name='ragan',
     packages=find_packages(),
     version='0.1.0',
     description='GANs applied to radiology images',
