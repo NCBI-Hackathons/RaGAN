8101b2d934a2a6320e17f3f7da43c5bf7a9113b3
p.samar.j@gmail.com <ubuntu@ip-172-16-243-193.ec2.internal>
2018-09-11 13:05:17 -0400

    update

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ This repository contains our work for the NCBI Hackathon organized 10-12 Sep, 20
 
 Day 1
 --------
+- Brainstorming on the scope of the project
+- Assigned the roles to team members
 
 Day 2
 --------
+- Created the cookiecutter project
+- Playing around with Generative Adversarial Network in tensorflow
+- Packaging the tool in Docker 
+- Adding jupyter notebook to the docker image
 
 Day 3
 --------
--- projectragan/ragan/__init__.py
+++ projectragan/ragan/__init__.py
@@ -1,1 +1,38 @@
-print("Hello World from RaGAN")
+import tensorflow as tf
+import numpy as np
+
+
+def get_y(x):
+    return 10 + x*x;
+
+def sample_data(n=10000, scale=100):
+    data = []
+
+    x = scale*(np.random.random_sample((n,))-0.5)
+
+    for i in range(n):
+        yi = get_y(x[i])
+        data.append([x[i], yi])
+
+    return np.array(data)
+
+def generator(Z, hsize=[16,16]):
+    with tf.variable_scope("GAN/Discriminator", reuse=False):
+        h1 = tf.layers.dense(Z,hsize[0], activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1], activation=tf.nn.leaky_relu)
+        out = tf.layers.dense(h2,2)
+    return out
+
+"""
+def discriminator(X, hsize=[16,16]):
+    with tf.variable_scope("GAN/Discriminator", reuse=reuse):
+        h1 = tf.layers
+"""
+
+def main():
+    print("Hello World from RaGAN")
+    a = sample_data()
+    Z = tf.placeholder(tf.float32, [None,2])
+    G_sample = generator(Z )
+
+main();
