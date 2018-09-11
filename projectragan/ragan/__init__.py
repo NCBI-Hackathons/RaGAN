20fb842d841c5375089df24ac87f5c8d3017124f
p.samar.j@gmail.com <ubuntu@ip-172-16-243-193.ec2.internal>
2018-09-11 15:01:02 -0400

    dense generator and discriminator implemented

--- projectragan/ragan/__init__.py
+++ projectragan/ragan/__init__.py
@@ -17,22 +17,28 @@ def sample_data(n=10000, scale=100):
     return np.array(data)
 
 def generator(Z, hsize=[16,16]):
-    with tf.variable_scope("GAN/Discriminator", reuse=False):
+    with tf.variable_scope("GAN/Generator", reuse=False):
         h1 = tf.layers.dense(Z,hsize[0], activation=tf.nn.leaky_relu)
         h2 = tf.layers.dense(h1,hsize[1], activation=tf.nn.leaky_relu)
         out = tf.layers.dense(h2,2)
     return out
 
-"""
-def discriminator(X, hsize=[16,16]):
+def discriminator(X, hsize=[16,16], reuse=False):
     with tf.variable_scope("GAN/Discriminator", reuse=reuse):
-        h1 = tf.layers
-"""
+        h1 = tf.layers.dense(X, hsize[0],activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
+        h3 = tf.layers.dense(h2,2)
+        out = tf.layers.dense(h3,1)
+    return out, h3
 
 def main():
     print("Hello World from RaGAN")
     a = sample_data()
+    X = tf.placeholder(tf.float32, [None,2])
     Z = tf.placeholder(tf.float32, [None,2])
     G_sample = generator(Z )
+    r_logits, r_rep = discriminator(X)
+    f_logits, g_rep = discriminator(G_sample, reuse=True)
 
+    
 main();
